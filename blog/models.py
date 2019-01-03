e73fee0f72447fbd28fc13347d460efed3fdc22b
mrbojan <bartlomiej.piotr.bojanowski@gmail.com>
2019-01-03 12:06:49 +0100

    Adding a data scheme

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from djnago.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	STATUS_CHOICE = (
+		('draft', 'Roboczy'),
+		('published', 'Opublikowany'),
+	)
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250,unique_for_date='publish')
+	author = models.ForeignKey(User,related_name='blog_posts')
+	body = models.TextField()
+	publish = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	update = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10,choice=Status_Choice,default='draft')
+	
+	class Meta:
+		ordering = ('-publish',)
+	def __str__(self):
+		return self.title
